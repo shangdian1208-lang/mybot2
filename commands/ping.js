57f6dcbe039e6ad762c25f99fdefcddf8d20ae6f
shangdian1208-lang <shangdian1208@gmail.com>
2026-03-14 17:17:07 +0800

    Update ping.js

--- commands/ping.js
+++ commands/ping.js
@@ -1,1 +1,11 @@
+const { SlashCommandBuilder } = require("discord.js");
 
+module.exports = {
+  data: new SlashCommandBuilder()
+    .setName("ping")
+    .setDescription("測試 Bot 延遲"),
+  async execute(interaction) {
+    const sent = await interaction.reply({ content: "Pinging...", fetchReply: true });
+    interaction.editReply(`延遲: ${sent.createdTimestamp - interaction.createdTimestamp}ms`);
+  }
+};
