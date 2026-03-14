8dc57af70757f14bc88e5d0dcc8259a4533e9822
shangdian1208-lang <shangdian1208@gmail.com>
2026-03-14 16:49:11 +0800

    Update setLogChannel.js

--- setLogChannel.js
+++ setLogChannel.js
@@ -1,10 +1,17 @@
-const { SlashCommandBuilder } = require("discord.js")
+const { SlashCommandBuilder } = require("discord.js");
+const db = require("../utils/database");
 
 module.exports = {
   data: new SlashCommandBuilder()
     .setName("set-log-channel")
-    .setDescription("設置日誌頻道"),
-  async execute(interaction){
-    interaction.reply("日誌頻道已設定！")
+    .setDescription("設置日誌頻道")
+    .addChannelOption(opt => opt.setName("channel").setDescription("選擇頻道").setRequired(true)),
+  async execute(interaction) {
+    const channel = interaction.options.getChannel("channel");
+    const data = db.readDB();
+    if (!data.settings) data.settings = {};
+    data.settings.logChannel = channel.id;
+    db.writeDB(data);
+    interaction.reply(`✅ 日誌頻道已設置為 ${channel}`);
   }
-}
+};
