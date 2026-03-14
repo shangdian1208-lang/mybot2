bf603f952e6a36a4408fa80b68987b139103eb61
shangdian1208-lang <shangdian1208@gmail.com>
2026-03-14 16:39:29 +0800

    Update database.js

--- database.js
+++ database.js
@@ -1,28 +1,10 @@
-const fs=require("fs")
+const fs = require("fs");
+const path = "./database.json";
 
-function loadDB(){
- return JSON.parse(fs.readFileSync("database.json"))
+function readDB() {
+  return JSON.parse(fs.readFileSync(path));
 }
-
-function saveDB(db){
- fs.writeFileSync("database.json",JSON.stringify(db,null,2))
+function writeDB(data) {
+  fs.writeFileSync(path, JSON.stringify(data, null, 2));
 }
-
-function getUser(db,id){
-
- if(!db.users[id]){
-
-  db.users[id]={
-   money:100,
-   xp:0,
-   level:1,
-   lastDaily:0
-  }
-
- }
-
- return db.users[id]
-
-}
-
-module.exports={loadDB,saveDB,getUser}
+module.exports = { readDB, writeDB };
