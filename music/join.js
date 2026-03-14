b6df5af0c3f520620317a9d19285473086d03c0b
shangdian1208-lang <shangdian1208@gmail.com>
2026-03-14 17:40:01 +0800

    Update join.js

--- music/join.js
+++ music/join.js
@@ -1,1 +1,17 @@
+const { SlashCommandBuilder } = require("discord.js");
+const { joinVoiceChannel } = require("@discordjs/voice");
 
+module.exports = {
+  data: new SlashCommandBuilder()
+    .setName("join")
+    .setDescription("加入語音頻道"),
+  async execute(interaction) {
+    if (!interaction.member.voice.channel) return interaction.reply("❌ 你必須在語音頻道中！");
+    joinVoiceChannel({
+      channelId: interaction.member.voice.channel.id,
+      guildId: interaction.guild.id,
+      adapterCreator: interaction.guild.voiceAdapterCreator
+    });
+    interaction.reply("✅ 已加入語音頻道！");
+  }
+};
