4a5c4b2768b255ccdf81c4065b0ed3772739e1ef
shangdian1208-lang <shangdian1208@gmail.com>
2026-03-14 16:37:17 +0800

    Update leaderboard.js

--- leaderboard.js
+++ leaderboard.js
@@ -1,30 +1,19 @@
-const {SlashCommandBuilder}=require("discord.js")
-const db=require("../database.json")
+const { SlashCommandBuilder } = require("discord.js");
+const db = require("../utils/database");
 
-module.exports={
-
-data:new SlashCommandBuilder()
-.setName("leaderboard")
-.setDescription("查看金錢排行榜"),
-
-async execute(interaction){
-
-const users=Object.entries(db.users)
-
-.sort((a,b)=>b[1].money-a[1].money)
-
-.slice(0,10)
-
-let text="🏆 金錢排行榜\n"
-
-users.forEach((u,i)=>{
-
- text+=`${i+1}. <@${u[0]}> ${u[1].money}💰\n`
-
-})
-
-interaction.reply(text)
-
-}
-
-}
+module.exports = {
+  data: new SlashCommandBuilder()
+    .setName("leaderboard")
+    .setDescription("查看金錢排行榜"),
+  async execute(interaction) {
+    const data = db.readDB();
+    const list = Object.entries(data.users)
+      .sort((a, b) => (b[1].money || 0) - (a[1].money || 0))
+      .slice(0, 10);
+    let text = "🏆 金幣排行榜\n";
+    list.forEach(([id, user], i) => {
+      text += `${i + 1}. <@${id}> - ${user.money || 0}💰\n`;
+    });
+    interaction.reply(text);
+  }
+};
