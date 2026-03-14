90820dcb2bf8d766ce7af88b9414032e0526d6c6
shangdian1208-lang <shangdian1208@gmail.com>
2026-03-14 16:37:33 +0800

    Update weather.js

--- weather.js
+++ weather.js
@@ -1,21 +1,23 @@
-const axios=require("axios")
+const { SlashCommandBuilder } = require("discord.js");
+const axios = require("axios");
 
-module.exports=(client)=>{
-
-setInterval(async()=>{
-
- const db=require("../database.json")
-
- if(!db.settings.weatherChannel) return
-
- const channel=client.channels.cache.get(db.settings.weatherChannel)
-
- const res=await axios.get(`https://api.openweathermap.org/data/2.5/weather?q=Taipei&appid=${process.env.WEATHER}`)
-
- const temp=(res.data.main.temp-273.15).toFixed(1)
-
- channel.send(`今日天氣：${temp}°C`)
-
-},3600000)
-
-}
+module.exports = {
+  data: new SlashCommandBuilder()
+    .setName("weather")
+    .setDescription("查詢城市天氣")
+    .addStringOption(option =>
+      option.setName("city").setDescription("輸入城市名稱").setRequired(true)
+    ),
+  async execute(interaction) {
+    const city = interaction.options.getString("city");
+    try {
+      const res = await axios.get(
+        `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${process.env.WEATHER}`
+      );
+      const temp = (res.data.main.temp - 273.15).toFixed(1);
+      interaction.reply(`🌦 ${city} 天氣\n溫度: ${temp}°C\n狀態: ${res.data.weather[0].description}`);
+    } catch (err) {
+      interaction.reply("查無此城市或 API 錯誤");
+    }
+  }
+};
