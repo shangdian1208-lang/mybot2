8b557232684fcef9738c7a76a144312d1ba20814
shangdian1208-lang <shangdian1208@gmail.com>
2026-03-14 16:56:03 +0800

    Update meme.js

--- meme.js
+++ meme.js
@@ -1,19 +1,18 @@
-const axios=require("axios")
+const axios = require("axios");
+const cron = require("node-cron");
+const db = require("../utils/database");
 
-module.exports=(client)=>{
-
-setInterval(async()=>{
-
- const db=require("../database.json")
-
- if(!db.settings.memeChannel) return
-
- const channel=client.channels.cache.get(db.settings.memeChannel)
-
- const res=await axios.get("https://meme-api.com/gimme")
-
- channel.send(res.data.url)
-
-},1800000)
-
-}
+module.exports = (client) => {
+  cron.schedule("*/30 * * * *", async () => { // 每 30 分鐘
+    const data = db.readDB();
+    if (!data.settings || !data.settings.memeChannel) return;
+    const channel = client.channels.cache.get(data.settings.memeChannel);
+    if (!channel) return;
+    try {
+      const res = await axios.get("https://some-random-api.ml/meme");
+      channel.send(res.data.caption + "\n" + res.data.image);
+    } catch (e) {
+      console.error("梗圖發送失敗:", e.message);
+    }
+  });
+};
