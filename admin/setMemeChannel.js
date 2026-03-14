876c37e7b04c4b8309164b8d4317df71ebe72f3e
shangdian1208-lang <shangdian1208@gmail.com>
2026-03-14 17:37:38 +0800

    Update setMemeChannel.js

--- admin/setMemeChannel.js
+++ admin/setMemeChannel.js
@@ -1,1 +1,17 @@
+const { SlashCommandBuilder } = require("discord.js");
+const db = require("../utils/database");
 
+module.exports = {
+  data: new SlashCommandBuilder()
+    .setName("set-meme-channel")
+    .setDescription("設置梗圖頻道")
+    .addChannelOption(opt => opt.setName("channel").setDescription("選擇頻道").setRequired(true)),
+  async execute(interaction) {
+    const channel = interaction.options.getChannel("channel");
+    const data = db.readDB();
+    if (!data.settings) data.settings = {};
+    data.settings.memeChannel = channel.id;
+    db.writeDB(data);
+    interaction.reply(`✅ 梗圖頻道已設置為 ${channel}`);
+  }
+};
