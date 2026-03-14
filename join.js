752141dbe2515020e2b29bf326e99f3d19c82449
shangdian1208-lang <shangdian1208@gmail.com>
2026-03-14 16:53:16 +0800

    Update join.js

--- join.js
+++ join.js
@@ -1,33 +1,17 @@
-const {SlashCommandBuilder}=require("discord.js")
-const {joinVoiceChannel}=require("@discordjs/voice")
+const { SlashCommandBuilder } = require("discord.js");
+const { joinVoiceChannel } = require("@discordjs/voice");
 
-module.exports={
-
-data:new SlashCommandBuilder()
-.setName("join")
-.setDescription("加入語音頻道"),
-
-async execute(interaction){
-
-const channel=interaction.member.voice.channel
-
-if(!channel){
-
- interaction.reply("請先加入語音頻道")
- return
-
-}
-
-joinVoiceChannel({
-
- channelId:channel.id,
- guildId:interaction.guild.id,
- adapterCreator:interaction.guild.voiceAdapterCreator
-
-})
-
-interaction.reply("已加入語音")
-
-}
-
-}
+module.exports = {
+  data: new SlashCommandBuilder()
+    .setName("join")
+    .setDescription("加入語音頻道"),
+  async execute(interaction) {
+    if (!interaction.member.voice.channel) return interaction.reply("❌ 你必須在語音頻道中！");
+    joinVoiceChannel({
+      channelId: interaction.member.voice.channel.id,
+      guildId: interaction.guild.id,
+      adapterCreator: interaction.guild.voiceAdapterCreator
+    });
+    interaction.reply("✅ 已加入語音頻道！");
+  }
+};
